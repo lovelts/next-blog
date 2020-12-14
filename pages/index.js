a7158d1a63be65021cb18c3bbeb50b731b797814
ltslyc <lts.lyc@qq.com>
2020-12-14 22:22:05 +0800

    优化布局2

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: lts
  * @Date: 2020-12-14 15:05:44
- * @LastEditTime: 2020-12-14 22:16:21
+ * @LastEditTime: 2020-12-14 22:21:23
  * @FilePath: \myblog\pages\index.js
  */
 import Head from 'next/head'
@@ -21,11 +21,11 @@ export default function Home() {
       </Head>
       <Header />
       <Row className="globals_main" type="flex" justify="center">
-        <Col className="globals_left" xs={24} sm={24} md={16} lg={14} xl={14}  >
+        <Col className="globals_left" xs={24} sm={24} md={18} lg={17} xl={14}>
           左侧
       </Col>
 
-        <Col className="globals_right" xs={0} sm={0} md={4} lg={4} xl={4}>
+        <Col className="globals_right" xs={0} sm={0} md={5} lg={4} xl={4}>
           右侧
       </Col>
       </Row>
